8eb8d8dfe7f9086e1632b263443ab7d5ceb61480
leconstantin <lcon69184@gmail.com>
2025-01-23 17:56:29 +0200

    added starter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import colors from "tailwindcss/colors";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray: colors.zinc,
+        "gray-1000": "rgb(17,17,19)",
+        "gray-1100": "rgb(10,10,11)",
+        vercel: {
+          pink: "#FF0080",
+          blue: "#0070F3",
+          cyan: "#50E3C2",
+          orange: "#F5A623",
+          violet: "#7928CA",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
